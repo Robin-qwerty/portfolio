e0a97b03f4dcbbc4ca1026677380d0c7fee97554
Robin <89701607+Robin-qwerty@users.noreply.github.com>
2022-06-28 10:50:04 +0200

    upgrading portfolio

--- include/home.inc.php
+++ include/home.inc.php
@@ -1,5 +1,5 @@
 <div id="home">
-  <p>- 👋 Hallo, ik ben Robin en doe nu de opleiding software development. -</p>
+  <p>- 👋 Hallo, ik ben <span id="robin">Robin</span> en doe nu de opleiding software development. -</p>
   <p>- 👀 Ik vind coderen erg leuk, daarom heb ik deze opleiding gekozen. -</p>
   <p>- 🌱 Ik ben op zoek naar een stage die te maken heeft met HTML, CSS & PHP. -</p>
   <p>- Ik heb daar nu al aardig wat mee heb gewerkt en ik me daar verder in wil verdiepen. -</p>
