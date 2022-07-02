53c4f3d6b76f34d3014bc961246b581f8de00cd7
Robin <89701607+Robin-qwerty@users.noreply.github.com>
2022-07-02 11:10:21 +0200

    mybuddy = done

--- MyBuddy/php/betalen.php
+++ MyBuddy/php/betalen.php
@@ -3,7 +3,7 @@
   session_start();
 
   try {
-    $sql = 'SELECT users.user_id, users.balans, leden.id, groepsbetalingen.bedrag, groepsbetalingen.id FROM users INNER JOIN leden ON leden.user = :user_id INNER JOIN groepsbetalingen ON groepsbetalingen.van_lid = leden.id WHERE users.user_id = :user_id AND groepsbetalingen.id = :id';
+    $sql = 'SELECT users.user_id, users.balans, leden.id, leden.groep, groepsbetalingen.bedrag, groepsbetalingen.id FROM users INNER JOIN leden ON leden.user = :user_id INNER JOIN groepsbetalingen ON groepsbetalingen.van_lid = leden.id WHERE users.user_id = :user_id AND groepsbetalingen.id = :id';
     $sth = $conn->prepare($sql);
     $sth->bindParam(':user_id', $_SESSION['user']);
     $sth->bindParam(':id', $_GET['page']);
@@ -23,9 +23,9 @@
         $sql = 'UPDATE groepen SET groepsbalans = groepsbalans+:bedrag WHERE groep_id = :groepsid';
         $sth = $conn->prepare($sql);
         $sth->bindParam(':bedrag', $row->bedrag);
-        $sth->bindParam(':groepsid', $_SESSION['groepsid']);
+        $sth->bindParam(':groepsid', $row->groep);
         $sth->execute();
-        
+
         $sql = 'UPDATE groepsbetalingen SET betaald = "yes" WHERE id = :id';
         $sth = $conn->prepare($sql);
         $sth->bindParam(':id', $row->id);
