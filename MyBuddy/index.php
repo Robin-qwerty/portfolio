f8aa9198ef566d21d2e8fb468fd26c413d4b676a
Robin <89701607+Robin-qwerty@users.noreply.github.com>
2022-11-04 16:13:43 +0100

    MyBuddy

--- MyBuddy/index.php
+++ MyBuddy/index.php
@@ -2,45 +2,44 @@
   if (isset($_GET['page'])) {
     $page = $_GET['page'];
   }
-?>
 
+  include'private/connections.php';
+  session_start();
+
+  $_SESSION['data'] = date('Y-m-d');
+
+?>
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset = "UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>home - Robin</title>
-    <link href="style/style.css" rel="stylesheet">
-    <script src="script/navscript.js" defer></script>
-    <script src="script/coding.js" defer></script>
-    <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.0-beta.3/dist/iconify-icon.min.js"></script>
-    <link href='https://fonts.googleapis.com/css?family=Chelsea Market' rel='stylesheet'>
+      <meta charset = "UTF-8">
+      <title>home - My Buddy</title>
+      <link href="css/style.css" rel="stylesheet">
+      <script src="script.js" defer></script>
   </head>
   <body>
+    <div class="main">
+    <?php include 'include/navbar.inc.php'; ?>
+    </div>
+
+    <div id="home">
+      <?php include 'include/topbar.inc.php';
+        //echo "<pre>", print_r($_SESSION),"</pre>";
+      ?>
+    </div>
 
     <?php
-    $home = 'index.php?page=home';
-
-    include 'include/navbar.inc.php';
-
-    try {
-      if (isset($page)) {
-        if ($page == 'footer' || $page == 'navbar') {
-          echo '<meta http-equiv="refresh" content="0; url='.$home.'" />';
-        } else {
-          include 'include/'.$page.'.inc.php';
-        }
+      if (isset($_GET['page'])) {
+        include 'include/'.$page.'.inc.php';
       } else {
         include 'include/home.inc.php';
       }
-    } catch (\Exception $e) {
-      echo '<meta http-equiv="refresh" content="0; url='.$home.'" />';
-    }
-
-    if (isset($page) && $page != 'home') {
-      include 'include/footer.inc.php';
-    }
     ?>
 
+    <div>
+      <footer>
+        <p>website made by Robin</p>
+      </footer>
+    </div>
   </body>
 </html>
