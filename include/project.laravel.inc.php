9f12795ee2f6fa5a6cb25d93fec483c05de088c4
Robin <89701607+Robin-qwerty@users.noreply.github.com>
2022-11-05 11:35:37 +0100

    Update project.laravel.inc.php

--- include/project.laravel.inc.php
+++ include/project.laravel.inc.php
@@ -1,7 +1,7 @@
 <h2>Laravel example-app</h2>
 <h4>programmeertalen: HTML, CSS &amp; PHP</h4>
 <h4>framework: Laravel</h4>
-<h3><a href="webshop/index.php" target="_blank" class="button">bekijk op GitHub</a> (dit project is nog niet af)</h3>
+<h3><a href="https://github.com/Robin-qwerty/laravel-example-app" target="_blank" class="button">bekijk op GitHub</a> (dit project is nog niet af)</h3>
 <p>
   Omdat veel stages laravel gebruiken en ik dit dus waarschijnlijk ook op mijn stage moet gaan gebruiken heb ik een klein simpel projectje gemaakt in laravel.
   Wat ik heb gemaakt is een simpele database met een relatie en die dan op de website geprint met 'events' er in en je kan deze toevoegen, aanpassen en verwijderen.
