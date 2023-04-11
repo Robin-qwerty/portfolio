776c1ba3abc65ac4d4c6cd370273f3f54c51c5e2
Robin <89701607+Robin-qwerty@users.noreply.github.com>
2023-04-11 16:01:15 +0200

    index

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home - Robin</title>
     <link href="style/style.css" rel="stylesheet">
-    <script src="script/navscript.js" defer></script>
-    <script src="script/coding.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.0-beta.3/dist/iconify-icon.min.js"></script>
     <link href='https://fonts.googleapis.com/css?family=Chelsea Market' rel='stylesheet'>
-  </head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@2000&display=swap" rel="stylesheet">  </head>
   <body>
 
     <?php
