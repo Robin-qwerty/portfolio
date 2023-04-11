6a9d6f2fc7c651b42b282d433c83832b57632816
Robin <89701607+Robin-qwerty@users.noreply.github.com>
2023-04-11 16:02:03 +0200

    Update navscript.js

--- script/navscript.js
+++ script/navscript.js
@@ -10,21 +10,3 @@ function togglemenu() {
     menulist.style.height = '60px';
   }
 }
-
-// console.log(window.innerWidth);
-//
-// window.addEventListener("resize", function() {
-//   if (window.innerWidth >= '920') {
-//     document.getElementById("home1").addEventListener("click", function() {
-//       if (menulist.style.height == '60px') {
-//           menulist.style.height = '100%';
-//       }
-//       else {
-//         menulist.style.height = '60px';
-//       }
-//     });
-//     console.log("Screen width is at least 920px")
-//   } else {
-//     console.log("Screen less than 920px")
-//   }
-// })
