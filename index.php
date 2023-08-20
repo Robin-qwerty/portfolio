bfc19cd5636638234d84da50443e2366432c02cb
Robin <89701607+Robin-qwerty@users.noreply.github.com>
2023-08-20 18:11:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,8 @@
     <link href='https://fonts.googleapis.com/css?family=Chelsea Market' rel='stylesheet'>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <!-- <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@2000&display=swap" rel="stylesheet">  </head> -->
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@2000&display=swap" rel="stylesheet"> -->
+  </head>
   <body>
 
     <?php
